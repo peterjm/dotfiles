0ce50ffc92993f12dab5c3fe8c24030ac054640c
Peter McCracken <peter.mccracken@jadedpixel.com>
2013-11-27 10:09:13 -0500

    allow for custom prompt colours

--- system/_bash/050_prompt.sh
+++ system/_bash/050_prompt.sh
@@ -1,5 +1,7 @@
 # If I am root, set the prompt to bright red
-if [ ${UID} -eq 0 ]; then PROMPT_COLOUR='31m' ; else PROMPT_COLOUR='33m' ; fi
+if [ ${UID} -eq 0 ]; then PROMPT_COLOUR='31m' ; fi
+# Default to yellow if the colour isn't already set
+if [[ -z "$PROMPT_COLOUR" ]]; then PROMPT_COLOUR='33m' ; fi
 # This sets the prompt to: "hostname top-level-directory[git_info] $"
 PS1='\[\033[01;${PROMPT_COLOUR}\]\h \[\033[01;${PROMPT_COLOUR}\]\W$(git_prompt_info '[%s]') \$ \[\033[00m\]'
 PS2='> '
