74a89f1967fd45901b3c8239374f212d1af9ccc2
Peter McCracken <peter.mccracken@jadedpixel.com>
2013-12-08 23:07:13 +0000

    use understandable bash colors

--- system/_bash/050_prompt.sh
+++ system/_bash/050_prompt.sh
@@ -1,9 +1,9 @@
 # If I am root, set the prompt to bright red
-if [ ${UID} -eq 0 ]; then PROMPT_COLOUR='31m' ; fi
+if [ ${UID} -eq 0 ]; then PROMPT_COLOUR=$Red ; fi
 # Default to yellow if the colour isn't already set
-if [[ -z "$PROMPT_COLOUR" ]]; then PROMPT_COLOUR='33m' ; fi
+if [[ -z "$PROMPT_COLOUR" ]]; then PROMPT_COLOUR=$Yellow ; fi
 # This sets the prompt to: "hostname top-level-directory[git_info] $"
-PS1='\[\033[01;${PROMPT_COLOUR}\]\h \[\033[01;${PROMPT_COLOUR}\]\W$(git_prompt_info '[%s]') \$ \[\033[00m\]'
+PS1="\[$PROMPT_COLOUR\]\h \W\$(git_prompt_info '[%s]') \$ \[$Color_Off\]"
 PS2='> '
 PS4='+ '
 export PS1 PS2 PS4
