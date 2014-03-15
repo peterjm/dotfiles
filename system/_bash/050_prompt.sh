f13d28b3880ae3dafbb81efd3d5f31a45f459421
Peter McCracken <peter@petermccracken.com>
2014-03-15 16:46:01 +0000

    allow prompt host to be directly set

--- system/_bash/050_prompt.sh
+++ system/_bash/050_prompt.sh
@@ -3,7 +3,8 @@ if [ ${UID} -eq 0 ]; then PROMPT_COLOUR=$Red ; fi
 # Default to yellow if the colour isn't already set
 if [[ -z "$PROMPT_COLOUR" ]]; then PROMPT_COLOUR=$Yellow ; fi
 # This sets the prompt to: "hostname top-level-directory[git_info] $"
-PS1="\[$PROMPT_COLOUR\]\h \W\$(git_prompt_info '[%s]') \$ \[$Color_Off\]"
+if [[ -z "$PROMPT_HOST" ]]; then PROMPT_HOST="\\h" ; fi
+PS1="\[$PROMPT_COLOUR\]$PROMPT_HOST \W\$(git_prompt_info '[%s]') \$ \[$Color_Off\]"
 PS2='> '
 PS4='+ '
 export PS1 PS2 PS4
