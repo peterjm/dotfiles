68069ad86500562168a59a10209d9950260b1f42
Peter McCracken <peter.mccracken@shopify.com>
2021-12-15 14:04:51 -0500

    update command line tool for interacting with light

--- system/lib/elgato/api_client.rb
+++ system/lib/elgato/api_client.rb
@@ -4,45 +4,103 @@ require 'net/http'
 require 'json'
 
 module Elgato
-  class ApiClient
-    LIGHTS_PATH = "/elgato/lights"
+  class Error < StandardError; end
 
-    class Error < StandardError; end
+  class Light
+    attr_reader :on, :brightness, :temperature
 
-    class Status
-      attr_reader :on, :brightness, :temperature
+    BRIGHTNESS_RANGE = (0..100)
+    TEMPERATURE_RANGE = (143..344)
 
-      class << self
-        def from_response(json_response)
-          light = json_response["lights"][0]
+    class << self
+      def max_brightness
+        BRIGHTNESS_RANGE.last
+      end
 
-          new(
+      def min_brightness
+        BRIGHTNESS_RANGE.first
+      end
+
+      def max_temperature
+        TEMPERATURE_RANGE.last
+      end
+
+      def min_temperature
+        TEMPERATURE_RANGE.first
+      end
+    end
+
+    def initialize(on: nil, brightness: nil, temperature: nil)
+      validate_brightness(brightness)
+      validate_temperature(temperature)
+
+      @on = on
+      @brightness = brightness
+      @temperature = temperature
+    end
+
+    private
+
+    def validate_brightness(value)
+      validate_range(value, BRIGHTNESS_RANGE, "Brightness")
+    end
+
+    def validate_temperature(value)
+      validate_range(value, TEMPERATURE_RANGE, "Temperature")
+    end
+
+    def validate_range(value, range, name)
+      return if value.nil?
+      return if range.include?(value)
+      raise Error, "#{name} must be between #{range.first} and #{range.last}"
+    end
+  end
+
+  class LightJSON
+    class << self
+      def serialize(light)
+        on_as_int = if light.on.nil?
+          nil
+        else
+          light.on ? 1 : 0
+        end
+
+        {
+          on: on_as_int,
+          brightness: light.brightness,
+          temperature: light.temperature,
+        }.compact
+      end
+
+      def deserialize(json)
+        json["lights"].map do |light|
+          Light.new(
             on: light["on"] == 1,
             brightness: light["brightness"],
             temperature: light["temperature"],
           )
         end
       end
-
-      def initialize(on:, brightness:, temperature:)
-        @on = on
-        @brightness = brightness
-        @temperature = temperature
-      end
     end
+  end
 
-    def initialize(ip_address: ENV["ELGATO_IP_ADDRESS"], port: 9123)
-      raise Error, "no ip_address specified" unless ip_address && ip_address != ""
+  class ApiClient
+    LIGHTS_PATH = "/elgato/lights"
+    OPEN_TIMEOUT = 1 # seconds
+    READ_TIMEOUT = 1 # seconds
+
+    def initialize(ip_address:, port: 9123)
+      raise Error, "No ip_address specified." unless ip_address && ip_address != ""
       @ip_address = ip_address
       @port = port
     end
 
     def status
       response = get(lights_uri)
-      Status.from_response(response)
+      LightJSON.deserialize(response).first
     end
 
-    def toggle_light
+    def toggle
       with_connection do |http|
         current_status = status
 
@@ -54,14 +112,24 @@ module Elgato
       end
     end
 
+    def set(on: nil, brightness: nil, temperature: nil)
+      update_light(Light.new(on: on, brightness: brightness, temperature: temperature))
+    end
+
     def turn_light_on
-      response = put(lights_uri, build_light_configuration_data(on: 1))
-      Status.from_response(response)
+      set(on: true)
     end
 
     def turn_light_off
-      response = put(lights_uri, build_light_configuration_data(on: 0))
-      Status.from_response(response)
+      set(on: false)
+    end
+
+    def set_brightness(brightness)
+      set(brightness: brightness)
+    end
+
+    def set_temperature(temperature)
+      set(temperature: temperature)
     end
 
     private
@@ -72,12 +140,15 @@ module Elgato
       URI::HTTP.build(host: ip_address, port: port, path: LIGHTS_PATH)
     end
 
-    def build_light_configuration_data(on:)
+    def update_light(settings)
+      response = put(lights_uri, build_light_configuration_data([settings]))
+      LightJSON.deserialize(response).first
+    end
+
+    def build_light_configuration_data(lights)
       {
-        numberOfLights: 1,
-        lights: [{
-          on: on,
-        }]
+        numberOfLights: lights.size,
+        lights: lights.map { |l| LightJSON.serialize(l) }
       }
     end
 
@@ -96,12 +167,12 @@ module Elgato
     end
 
     def with_connection
-      Net::HTTP.start(ip_address, port, open_timeout: 1, read_timeout: 1) do |connection|
+      Net::HTTP.start(ip_address, port, open_timeout: OPEN_TIMEOUT, read_timeout: READ_TIMEOUT) do |connection|
         @connection = connection
         yield
       end
     rescue Errno::EHOSTDOWN, Net::OpenTimeout
-      raise Error, "couldn't connect to lights"
+      raise Error, "Couldn't connect to lights."
     ensure
       @connection = nil
     end
