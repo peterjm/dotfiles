7904e86330b3b17ff8fa314f4e7e0de96ea293b1
Peter McCracken <peter@petermccracken.com>
2021-12-23 12:19:40 -0500

    homebrew prefix is stored in HOMEBREW_PREFIX

--- system/_zsh/050_auto_switch_ruby_version.sh
+++ system/_zsh/050_auto_switch_ruby_version.sh
@@ -1,4 +1,2 @@
-if type brew &>/dev/null; then
-  FILENAME="$(brew --prefix)/opt/chruby/share/chruby/auto.sh"
-  [ -f $FILENAME ] && source $FILENAME
-fi
+FILENAME="$HOMEBREW_PREFIX/opt/chruby/share/chruby/auto.sh"
+[ -f $FILENAME ] && source $FILENAME
--- system/_zsh/050_completions.sh
+++ system/_zsh/050_completions.sh
@@ -1,6 +1,4 @@
-if type brew &>/dev/null; then
-  FPATH=$(brew --prefix)/share/zsh-completions:$FPATH
+FPATH=$HOMEBREW_PREFIX/share/zsh-completions:$FPATH
 
-  autoload -Uz compinit
-  compinit
-fi
+autoload -Uz compinit
+compinit
