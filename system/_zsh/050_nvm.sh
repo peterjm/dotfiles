e3837c810c66e21decb9363bf624d59ee8828840
Peter McCracken <peter@petermccracken.com>
2022-01-04 11:30:43 -0500

    add nvm

---
+++ system/_zsh/050_nvm.sh
@@ -0,0 +1,4 @@
+export NVM_DIR="$HOME/.nvm"
+
+source_if_exists "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" # This loads nvm
+#source_if_exists "$HOMEBREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
